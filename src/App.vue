d3b606ee64d6896bde2f1df26d5623267207a03c
Kai Qin <qq554552354@126.com>
2021-12-03 22:06:13 +0800

    tmp save

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,49 @@
 <template>
-  <img src="./assets/logo.png">
   <div>
-    <p>
-      If Element Plus is successfully added to this project, you'll see an
-      <code v-text="'<el-button>'"></code>
-      below
-    </p>
-    <el-button type="primary">el-button</el-button>
+    <TitleBar :user_id="user_info.user_id" :phone="user_info.phone"/>
+    <el-main>
+      test main
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </el-main>
   </div>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TitleBar from "./components/TitleBar.vue";
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TitleBar
+  },
+  data() {
+    return {
+      user_info: {
+        user_id: "",
+        phone: ""
+      }
+    }
+  },
+  mounted() {
+    this.get_user_name();
+  },
+  methods: {
+    async get_user_name() {
+      console.log("async test");
+      let res = await axios.get("http://127.0.0.1:5000/api/users/user_info", {
+        params: {
+          user_id: "test_user"
+        }
+      })
+
+      console.log(res.data);
+      this.user_info.user_id = res.data.user_id;
+      this.user_info.phone = res.data.phone;
+    }
   }
 }
 </script>
--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -1,14 +1,46 @@
 <template>
   <div>
-    test bar
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" text-color="#000000">
+      <el-menu-item index="1">
+        <el-row>
+          <el-col :span="8">
+            <el-avatar :size="medium" :src="circleUrl"/>
+          </el-col>
+          <el-col :span="16">
+            <el-col align="center" style="text-align: center; font-size: 2em">
+              {{user_id}}
+            </el-col>
+          </el-col>
+        </el-row>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <div class="additional_info">
+          phone: {{phone}}
+        </div>
+      </el-menu-item>
+    </el-menu>
   </div>
 </template>
 <script>
 export default {
-name: "TitleBar"
+  name: "TitleBar",
+  props: {
+    user_id: String,
+    phone: String
+  },
+  data() {
+    return {
+      activeIndex: 1,
+      circleUrl: "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+    }
+  }
 }
 </script>
 
 <style scoped>
+
+.additional_info {
+  font-size: 2em
+}
 
 </style>
